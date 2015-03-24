1115fe73ba0a0823b9a52bd3f1ed885d79b5096a
Youhei SASAKI <uwabami@gfd-dennou.org>
2015-03-24 20:45:53 +0900

    Update jekyll
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ js/diary.js
@@ -0,0 +1,8 @@
+document.writeln('<h3><a href="http://uwabami.junkhub.org/log/">平衡点</a>(blog): Recent entries</h3><ul>');document.close();
+document.writeln('<li class="RSSitems"><a href="http://uwabami.junkhub.org/log/20150320.html#p01">Wanderlust で分割メールを扱う</a></li>');document.close();
+document.writeln('<li class="RSSitems"><a href="http://uwabami.junkhub.org/log/20150317.html#p01">Subversion から Git への移行</a></li>');document.close();
+document.writeln('<li class="RSSitems"><a href="http://uwabami.junkhub.org/log/20150316.html#p01">tDiary に bootstrap >= 3 を使う</a></li>');document.close();
+document.writeln('<li class="RSSitems"><a href="http://uwabami.junkhub.org/log/20150303.html#p01">VPS の Debian を Jessie に上げた</a></li>');document.close();
+document.writeln('<li class="RSSitems"><a href="http://uwabami.junkhub.org/log/20150102.html#p01">あけましておめでとうございます．</a></li>');document.close();
+document.writeln('<li class="RSSitems"><a href="http://uwabami.junkhub.org/log/20141220.html#p01">モードラインのダイエット (2/n)</a></li>');document.close();
+document.writeln('</ul>');document.close();
