f37668dc587a7476c70640eee39c3e95e698c8c8
Youhei SASAKI <uwabami@gfd-dennou.org>
2017-01-19 03:25:52 +0900

    Update webfont icons

--- assets/js/org-toc.js
+++ assets/js/org-toc.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
     $('#text-table-of-contents ul').first().addClass('nav');
     // ScrollSpy also requires that we use a Bootstrap nav component.
-    $('body').scrollspy({target: '#table-of-contents'});
+    $('body').scrollspy({target: '#table-of-contents', offset:90 });
+    $("h2", "#table-of-contents").wrapInner('<a  /a>');
     $("#toc-toggle").click(function(e){
         e.preventDefault();
         $("#toc-wrapper").toggleClass("toggled");
